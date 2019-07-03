e3e2db5f3225b84d30cda69478cd258f2aea654d
Chris Junker <elevatemedfit@gmail.com>
2019-07-03 13:48:21 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,26 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
+
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand))
+    BRANDS << brand
   end
+end
+
+  #def unique(brand)
+    #BRANDS.clear.flatten << brand.uniq
+    #BRANDS.size
+    #def initialize(brand)
+    #@brand = brand
+    #if !(BRANDS.include?(@brand))
+      #BRANDS << @brand
+    #end
+  #end
+  #end
 
   def cobble
     self.condition = "new"
